8528abe6f50dce2f79483d8953ebd974bac9c5e5
henriqueannicchino <henriquematos285@gmail.com>
2025-02-23 19:15:56 +0100

    feat: added endpoints

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,66 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_should_return_ok_hellor_world():
-    client = TestClient(app)  # Arrange
-
+def test_read_root_should_return_ok_hellor_world(client):
     response = client.get("/")  # Act
 
     assert response.status_code == HTTPStatus.OK  # assert
     assert response.json() == {"message": "Hello world!"}  # assert
+
+
+def test_create_user(client):
+    response = client.post(  # UserSchema
+        "/users/",
+        json={
+            "username": "testusername",
+            "password": "password",
+            "email": "test@test.com",
+        },
+    )
+
+    # Returned the right code status?
+    assert response.status_code == HTTPStatus.CREATED
+
+    # Validate UserPublic
+    assert response.json() == {
+        "username": "testusername",
+        "email": "test@test.com",
+        "id": 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get("/users/")
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        "users": [
+            {
+                "username": "testusername",
+                "email": "test@test.com",
+                "id": 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        "/users/1",
+        json={
+            "password": "123",
+            "username": "testusername2",
+            "email": "test@test.com",
+            "id": 1,
+        },
+    )
+    assert response.json() == {
+        "username": "testusername2",
+        "email": "test@test.com",
+        "id": 1,
+    }
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message', 'User deteleted'}
